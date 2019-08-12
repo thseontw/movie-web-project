3e2d69f355fdadc37e17859d0929eca85fdc3ad6
seomkim <thseontw@gmail.com>
2019-08-12 23:00:03 +0900

    edit text test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Make Movie it!
         </a>
       </header>
     </div>
